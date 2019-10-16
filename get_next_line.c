83dce59ccd1ad632b87af9342e681a0a993295b6
Solal Dunckel <sdunckel@student.42.fr>
2019-10-16 12:03:42 +0200

    fix error

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sdunckel <sdunckel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 11:16:56 by sdunckel          #+#    #+#             */
-/*   Updated: 2019/10/15 12:50:13 by sdunckel         ###   ########.fr       */
+/*   Updated: 2019/10/16 12:02:23 by sdunckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 int		get_next_line(int fd, char **line)
 {
-	static char		*s[256];
+	static char		*s[10240];
 	char			buf[BUFFER_SIZE + 1];
 	int				ret;
 	char			*tmp;
 
-	if (fd < 0 || !line || BUFFER_SIZE < 1)
+	if (fd < 0 || !line || BUFFER_SIZE < 1 || read(fd, buf, 0) < 0)
 		return (ERROR);
-	if (!s[fd])
-		if (!(s[fd] = malloc(sizeof(char *))))
-			return (ERROR);
+	if (!s[fd] && !(s[fd] = malloc(sizeof(char *))))
+		return (ERROR);
 	while ((is_in_s('\n', s[fd])) < 0 && (ret = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
@@ -31,7 +30,8 @@ int		get_next_line(int fd, char **line)
 		s[fd] = ft_strjoin(s[fd], buf);
 		free(tmp);
 	}
-	*line = ft_substr(s[fd], 0, ft_strlen_gnl(s[fd]) + 1);
+	if (ret > 0 || ft_strlen_gnl(s[fd]) > 0)
+		*line = ft_substr(s[fd], 0, ft_strlen_gnl(s[fd]));
 	if (is_in_s('\n', s[fd]) >= 0)
 		s[fd] = ft_substr(s[fd], is_in_s('\n', s[fd]) + 1, ft_strlen(s[fd]));
 	else if (ft_strlen_gnl(s[fd]) > 0)
