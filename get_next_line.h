842fb1a6071ef9a4f7b89aa4fca7f231307ae9ca
Solal Dunckel <sdunckel@student.42.fr>
2019-10-16 15:59:59 +0200

    pouloulou

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,17 @@
 /*   By: sdunckel <sdunckel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 11:17:21 by sdunckel          #+#    #+#             */
-/*   Updated: 2019/10/15 12:35:24 by sdunckel         ###   ########.fr       */
+/*   Updated: 2019/10/16 14:19:53 by sdunckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 4
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
 # define SUCCESS 1
 # define FINISH 0
 # define ERROR -1
@@ -24,8 +27,6 @@
 # include <fcntl.h>
 # include <unistd.h>
 
-int		get_next_line(int fd, char **line);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *s);
 int		ft_strlen_gnl(char *s);
 int		is_in_s(char c, char *str);
